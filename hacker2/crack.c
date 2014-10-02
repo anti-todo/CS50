97b0f571fb4260438b343bd1505778ddd7509f55
jharvard <anti-todo>
2014-10-02 10:36:29 -0700

    added comments

--- hacker2/crack.c
+++ hacker2/crack.c
@@ -3,7 +3,7 @@
  *
  * CS50 hacker2
  *
- * To do: -comment
+ * To do:
  *        -add bruteforce routine to run after dictionary attack
  */
 
@@ -11,7 +11,6 @@
 #include <unistd.h>
 #define _GNU_SOURCE
 #include <crypt.h>
-#include <cs50.h>
 #include <string.h>
 #include <stdio.h>
 
@@ -32,34 +31,50 @@ int main(int argc, char* argv[])
     else
     {
         char salt[3];
+        // copy first 2 chars of the argument to salt array, avoid pointer folly
         memcpy(salt, argv[1], 2);
+        // terminate with null char, makes a real string, avoid trailing garbage
         salt[2] = '\0';
         char word[100];
         char * hashed;
+        // declare a file pointer
         FILE * wordList;
 
+        // assign it to to a dictionary file on HDD using fopen()
         wordList = fopen("/usr/share/dict/words", "r");
-        //wordList = fopen("/home/jharvard/Downloads/rockyou.txt", "r");
+        
+        // rockyou wordlist finds works a lot better than dict/words
+        // wordList = fopen("/home/jharvard/Downloads/rockyou.txt", "r");
+        
+        // Handle situation where the file is not found
         if (!wordList)
         {
             printf("word list not found\n");
             return 2;
         }
+        
+        // use fgets get string from file 1 line at a time while if no error (null)
         while (fgets(word, 100, wordList) != NULL)
         {
+            // replace the newline char that fgets returns with null char
             unsigned int length = strlen(word) - 1;
             if (word[length] == '\n')
             {
                 word[length] = '\0';
             }
+            // assign the result of des algo on dict word and salt to variable
             hashed = crypt(word, salt);
+            // compare that hashed result with the user supplied hash
             if (strcmp(hashed, argv[1]) == 0)
             {
+                // if hashes match (strcmp returns 0), print the word from dict
                 printf("%s\n", word);
+                // close the file and exit the program
                 fclose(wordList);
                 return 0;
             }
         }
+        // if nothing is found close the file and exit the program
         fclose(wordList);
         return 0;
     }
